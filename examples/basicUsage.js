6aa1493dba28d0fe00d493efd9c0aeba9e19e4a8
Sefinek <contact@sefinek.net>
2024-08-15 04:31:36 +0200

    Some fixes

--- examples/basicUsage.js
+++ examples/basicUsage.js
@@ -1,6 +1,6 @@
 const NekosiaAPI = require('../index.js');
 
 (async () => {
-	const json = await NekosiaAPI.fetchCatgirlImages();
+	const json = await NekosiaAPI.fetchFoxgirlImages({ session: 'ip', count: 1, additionalTags: ['cute', 'sakura', 'cherry-blossom'], blacklistedTags: ['beret'] });
 	console.log(json);
 })();
--- index.js
+++ index.js
@@ -3,58 +3,63 @@ const categories = require('./categories.js');
 
 class NekosiaAPI {
 	constructor() {
-		this.baseURL = 'https://api.nekosia.cat';
+		this.baseURL = 'https://api.nekosia.cat/api/v1';
 		this.initializeCategoryMethods();
 	}
 
 	initializeCategoryMethods() {
 		categories.forEach(category => {
-			const methodName = this.buildMethodName(category);
-			this[methodName] = (options = {}) => this.fetchImagesByCategory(category, options);
+			this[this.buildMethodName(category)] = options => this.fetchImagesByCategory(category, options);
 		});
 	}
 
 	buildMethodName(category) {
-		return `fetch${category.charAt(0).toUpperCase() + category.slice(1).replace(/-/g, '')}Images`;
+		return `fetch${category.replace(/-/g, ' ').replace(/\b\w/g, char => char.toUpperCase()).replace(/\s/g, '')}Images`;
 	}
 
-	buildQueryParams(options) {
-		return new URLSearchParams(Object.entries(options).filter(([, value]) => value != null && value !== '')).toString();
+	buildQueryParams(options = {}) {
+		return Object.entries(options)
+			.filter(([, value]) => value != null && value !== '' && (!Array.isArray(value) || value.length))
+			.map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(String(value))}`)
+			.join('&');
 	}
 
 	async makeHttpRequest(endpoint) {
 		try {
 			return await https.get(endpoint);
 		} catch (err) {
-			console.error(`HTTP request failed for endpoint ${endpoint}:`, err.message);
+			console.error(`HTTP request failed for endpoint ${endpoint}: ${err.message}`);
 			throw err;
 		}
 	}
 
 	fetchImagesByCategory(category, options = {}) {
-		const finalOptions = {
+		const queryString = this.buildQueryParams({
 			session: null,
 			id: null,
 			count: 1,
-			additionalTags: '',
-			blacklistedTags: '',
+			additionalTags: [],
+			blacklistedTags: [],
 			...options
-		};
-
-		const queryString = this.buildQueryParams(finalOptions);
-		return this.makeHttpRequest(`${this.baseURL}/api/v1/images/${category}?${queryString}`);
+		});
+		return this.makeHttpRequest(`${this.baseURL}/images/${category}?${queryString}`);
 	}
 
 	async fetchShadowImages(additionalTagsArray = [], options = {}) {
-		if (!additionalTagsArray.length) throw new Error('additionalTagsArray must be a non-empty array for the shadow category.');
+		if (!additionalTagsArray.length) {
+			throw new Error('`additionalTagsArray` must be a non-empty array for the shadow category');
+		}
 
-		const additionalTags = additionalTagsArray.join(',');
-		return this.fetchImagesByCategory('shadow', { ...options, additionalTags });
+		return this.fetchImagesByCategory('shadow', {
+			...options,
+			additionalTags: additionalTagsArray.join(',')
+		});
 	}
 
 	async fetchById(id) {
-		if (!id) throw new Error('id parameter is required.');
-		return this.makeHttpRequest(`${this.baseURL}/api/v1/getImageById/${id}`);
+		if (!id) throw new Error('`id` parameter is required');
+
+		return this.makeHttpRequest(`${this.baseURL}/getImageById/${id}`);
 	}
 }
 
