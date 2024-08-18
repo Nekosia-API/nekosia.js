ab349f088149bad0ee0ab732bb1fbf2b1938e074
Sefinek <contact@sefinek.net>
2024-08-18 17:42:16 +0200

    Delete file

--- categories.js
+++
@@ -1,9 +0,0 @@
-module.exports = [
-	'catgirl', 'foxgirl', 'wolf-girl', 'animal-ears', 'tail', 'tail-with-ribbon',
-	'tail-from-under-skirt', 'cute', 'cuteness-is-justice', 'blue-archive', 'girl',
-	'young-girl', 'maid', 'maid-uniform', 'vtuber', 'w-sitting', 'lying-down',
-	'hands-forming-a-heart', 'wink', 'valentine', 'headphones', 'thigh-high-socks',
-	'knee-high-socks', 'white-tights', 'black-thighs', 'heterochromia', 'uniform',
-	'sailor-uniform', 'hoodie', 'ribbon', 'white-hair', 'blue-hair', 'long-hair',
-	'blonde', 'blue-eyes', 'purple-eyes', 'swimwear', 'swimsuit', 'bikini', 'sea', 'swim-ring'
-];
